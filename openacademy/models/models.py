e68a0f434057e6a7ae1089c4e0e7459a17958d2e
Manuel Topete Ortega <manuel@vauxoo.com>
2018-01-12 12:30:12 -0600

    [REF] Inheritances

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -10,7 +10,6 @@ class Course(models.Model):
     responsible_id = fields.Many2one(
     	'res.users',string="Responsible",
     	index=True, ondelete='set null')
-
     session_ids = fields.One2many('openacademy.session','course_id')
 
 class Session(models.Model):
@@ -20,7 +19,7 @@ class Session(models.Model):
 	start_date = fields.Date()
 	duration = fields.Float(digits=(6, 2), help="Duration in days")
 	seats = fields.Integer(string="Number of seats")
-	instructor_id = fields.Many2one('res.partner',string='Instructor')
+	instructor_id = fields.Many2one('res.partner',string='Instructor', domain=[('instructor','=',True)])
 	course_id = fields.Many2one('openacademy.course', ondelete='cascade',
 	 string="Course", required=True)
-	attendee_ids = fields.Many2one('res.partner',string="attendees")
+	attendee_ids = fields.Many2many('res.partner',string="attendees")
