2dbdaea641819144a984a6f5acb8b39c7e7764b2
Manuel Topete Ortega <manuel@vauxoo.com>
2018-01-12 16:01:59 -0600

    [REF] Widgets

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -9,7 +9,8 @@ class Course(models.Model):
     description = fields.Text()
     responsible_id = fields.Many2one(
     	'res.users',string="Responsible",
-    	index=True, ondelete='set null')
+    	index=True, ondelete='set null',
+    	default=lambda self, *a : self.env.uid)
     session_ids = fields.One2many('openacademy.session','course_id')
 
 class Session(models.Model):
@@ -17,9 +18,18 @@ class Session(models.Model):
 
 	name = fields.Char(required=True)
 	start_date = fields.Date()
+	datetime_test = fields.Datetime(default=fields.Datetime.now)
 	duration = fields.Float(digits=(6, 2), help="Duration in days")
 	seats = fields.Integer(string="Number of seats")
 	instructor_id = fields.Many2one('res.partner',string='Instructor', domain=[('instructor','=',True)])
 	course_id = fields.Many2one('openacademy.course', ondelete='cascade',
 	 string="Course", required=True)
 	attendee_ids = fields.Many2many('res.partner',string="attendees")
+	taken_seats = fields.Float(compute='_taken_seats')
+	active = fields.Boolean(default=True)
+
+	@api.depends('seats','attendee_ids')
+	def _taken_seats(self):
+		# import pdb; pdb.set_trace()
+		for record in self.filtered(lambda r: r.seats != 0 ):
+			record.taken_seats = 100.0 * len(record.attendee_ids) / record.seats
