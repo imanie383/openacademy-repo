f385e774a7d974cb5989bb7b45df925a05cd15a7
Manuel T. Vauxoo <manuel@vauxoo.com>
2018-02-18 16:18:48 -0600

    [ADD] Added wizard to subscribe attendees

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -3,38 +3,35 @@
 from odoo import models, fields, api, exceptions
 from psycopg2 import IntegrityError
 
+def get_uid(self, *a):
+    return self.env.uid
+
 class Course(models.Model):
     _name = 'openacademy.course'
 
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
-    responsible_id = fields.Many2one(
-    	'res.users',string="Responsible",
-    	index=True, ondelete='set null',
-    	default=lambda self, *a : self.env.uid)
-    session_ids = fields.One2many('openacademy.session','course_id')
+    responsible_id = fields.Many2one('res.users', string="Responsible",
+         index=True, ondelete='set null',
+         default = get_uid )
+    session_ids = fields.One2many('openacademy.session', 'course_id')
 
     _sql_constraints = [
-    	('name_description_check', 'CHECK(name != description)', 
-    		"The title of the course shouldn't be the description" ), 
-    	('name_unique', 'UNIQUE(name)', "The course title must be unique" )
+        ('name_description_check', 'CHECK(name != description)',
+         "The title of the course shouldn't be the description" ),
+
+        ('name_unique', 'UNIQUE(name)',
+            "The course title must be unique" )
     ]
 
     def copy(self, default=None):
-    	 if default is None:
-    	 	default = {}
-    	 copied_count = self.search_count(
-	    	 	[
-	    	 		('name','ilike','Copy of %s%%' % (self.name))
-	    	 	]
-	    	 )
-
-    	 if not copied_count:
-    	 	new_name = "Copy of %s" % (self.name)
-    	 else:
-    	 	new_name = "Copy of %s (%s)" % (self.name, copied_count)
-
-    	 default['name'] = new_name
-    	 # try:
-    	 return super(Course, self).copy(default)
-    	 # except exception
+        if default is None:
+            default = {}
+        copied_count = self.search_count([
+            ('name', 'ilike', _('Copy of %s%%') % (self.name))])
+        if not copied_count:
+            new_name = _("Copy of %s") % (self.name)
+        else:
+            new_name = _("Copy of %s (%s)")%(self.name, copied_count)
+        default['name'] = new_name
+        return super(Course, self).copy(default)
